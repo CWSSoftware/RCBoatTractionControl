6de19e2e5a6a6f9b8c12813e8d8df1c281151fc7
Charlotte <charlottewisteria@gmail.com>
2017-04-17 19:32:03 -0700

    Created Eclipse project and main class

---
+++ src/RCBoatTractionControl.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Charlotte
+ *
+ */
+public class RCBoatTractionControl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
